0d32ade0985369b19e59a6cad31f9edede160871
chrishalcrow <57948917+chrishalcrow@users.noreply.github.com>
2024-01-24 09:31:02 +0100

    Add arrow plot

--- src/plotting.jl
+++ src/plotting.jl
@@ -27,3 +27,48 @@ function plot_energy(sk)
 
 end
 
+
+function sk_arrows(P0::Skyrmion; SFreq=1, cut=0)
+
+    Nx = P0.lp[1]
+    Ny = P0.lp[2]
+
+    xlr = P0.x[1][1+cut:SFreq:Nx-cut]
+    ylr = P0.x[2][1+cut:SFreq:Ny-cut]
+
+    Plr = P0.field[1+cut:SFreq:Nx-cut,1+cut:SFreq:Ny-cut,1:3]
+
+    strength = zeros( size(Plr)[1], size(Plr)[2] )
+    angles = zeros(HSL{Float64}, size(Plr)[1], size(Plr)[2] )
+
+    for i in 1:size(Plr)[1], j in 1:size(Plr)[2]
+        strength[i,j] = exp(-0.01*(Plr[i,j,1]^2 + Plr[i,j,2]^2))
+        angles[i,j] = HSL(atand(Plr[i,j,2], Plr[i,j,1]), 1, 0.5 + 0.5*Plr[i,j,3] )
+        #Plr[i,j,1:2] = normalize(Plr[i,j,1:2])
+    end
+
+    strength = vec(strength)
+    angles = vec(angles)
+
+    ps = [Point3f(xlr[i], ylr[j],0.0) for i in 1:size(xlr)[1] for j in 1:size(ylr)[1] ];
+    ns = [Point3f(Plr[i,j,1],Plr[i,j,2],Plr[i,j,3]) for i in 1:size(xlr)[1] for j in 1:size(ylr)[1] ];
+
+    fig = Figure()
+
+    axs = [Axis3(fig[1, i];
+        #xgridvisible = false,
+        #ygridvisible = false    
+        elevation = pi/2.0,
+        azimuth = 0.0,
+        perspectiveness=0.0
+        ) for i=1:1]
+
+    hidedecorations!(axs[1])
+
+    arrows!(axs[1], ps, ns, arrowcolor = angles, linecolor = angles, lengthscale=0.01, arrowsize=0.1*SFreq)
+
+    zlims!(-1,1)
+
+    return fig
+
+end
